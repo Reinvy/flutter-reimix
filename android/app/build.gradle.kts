683d6db6f9ef2f35a83df6b91658542333f1708b
Reinvy <reinvy.dev@gmail.com>
2026-04-10 12:13:53 +0700

    feat: Update Android and iOS configurations, integrate OnAudioQuery for media access, and remove deprecated audio metadata handling

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -62,6 +63,12 @@ android {
             )
         }
     }
+}
+
+dependencies {
+    implementation("androidx.window:window:1.0.0")
+    implementation("androidx.window:window-java:1.0.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
